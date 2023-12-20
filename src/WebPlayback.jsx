5eb96851836631ee78b2449be232eeb9aa4a70fa
watermelonPop <SK21007@Tamu.edu>
2023-12-19 19:45:02 -0600

    new changes

--- src/WebPlayback.jsx
+++ src/WebPlayback.jsx
@@ -1005,7 +1005,7 @@ function WebPlayback(props) {
                         }}
                       />
                     </Button>
-                      <div style={{marginTop: "10vh"}}>
+                      <div style={{marginTop: "20vh"}}>
                       <Typography component="h1" variant="h1" color="textPrimary" textAlign="center" gutterBottom>
                         Customize Theme
                       </Typography>
